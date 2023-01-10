82ab6ccdea59ade3b20ff3cf319c871a40357db5
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-01-10 16:43:25 +0200

    add state and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,42 @@
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+import Home from './page/home/Home';
+import ItemDetails from './page/itemDetails/ItemDetails'
+import Checkout from './page/checkout/Checkout';
+import Confirmation from './page/checkout/Confirmation'
+import Navbar from './page/global/Navbar';
 
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
-  return 
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Navbar/>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="confirmation" element={ <Confirmation/> } />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './state'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { theme } from './theme';
 
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
